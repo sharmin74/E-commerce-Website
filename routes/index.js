a9307f6f1fbb482ed5f7caac4caabe0dbfd4563a
Sharmin Oomatia <sharminoomatia205@gmail.com>
2020-08-27 10:11:37 +0530

    made shopping cart and index page

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,14 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+
+router.get('/product-information', (req, res) => {
+    res.render('product-info');
+});
+
+
+router.get('/shopping-cart', (req, res) => {
+    res.render('shoppingCart');
+});
+
 module.exports = router;
